123939a1d611ece7930f9d34abdcbd7e80135cab
lilyvll <lilliankline@seattleacademy.org>
2024-06-07 14:24:30 -0700

    Color quiz

--- Project 15 - Quiz Website/example/app.js
+++ Project 15 - Quiz Website/example/app.js
@@ -2,54 +2,135 @@
 'use strict';
 
 function resetResult() {
-    document.getElementById("LAResult").style.display = "none";
-    document.getElementById("SeattleResult").style.display = "none";
-    document.getElementById("MiamiResult").style.display = "none";
-    document.getElementById("ChicagoResult").style.display = "none";
+    document.getElementById("RedResult").style.display = "none";
+    document.getElementById("GreenResult").style.display = "none";
+    document.getElementById("BlueResult").style.display = "none";
+    document.getElementById("OrangeResult").style.display = "none";
+    document.getElementById("YellowResult").style.display = "none";
+    document.getElementById("PurpleResult").style.display = "none";
+    document.getElementById("PinkResult").style.display = "none";
 }
 
 function seeResult() {
-    var LA = 0;
-    var Seattle = 0;
-    var Miami = 0;
-    var Chicago = 0;
+    var Red = 0;
+    var Green = 0;
+    var Blue = 0;
+    var Orange = 0;
+    var Yellow = 0;
+    var Purple = 0;
+    var Pink = 0;
 
-    if (document.getElementById("hot").checked) {
-        LA += 1;
-        Miami += 1;
-    } else if (document.getElementById("cold").checked) {
-        Chicago += 1;
+    if (document.getElementById("warm").checked) {
+        Red += .5;
+        Orange += .5;
+        Yellow += .5;
+        Pink += .5;
+    } else if (document.getElementById("cool").checked) {
+        Green += .5;
+        Blue += .5;
+        Purple += .5;
     } else {
-        Seattle += 1;
+        Red += 0;
     }
 
-    if (document.getElementById("bus").checked) {
-        Miami += 1;
-    } else if (document.getElementById("car").checked) {
-        LA += 1;
+    if (document.getElementById("everyone ever").checked) {
+        Blue += 1;
+        Purple += 1;
+    } else if (document.getElementById("eh").checked) {
+        Green += 1;
+        Red += 1;
     } else {
-        Seattle += 1;
-        Chicago += 1;
+        Yellow += 2;
+        Orange += 2;
+        Pink += 1;
     }
 
-    if (document.getElementById("walk").checked) {
-        Seattle += 1;
-        Miami += 1;
-    } else if (document.getElementById("drive").checked) {
-        LA += 1;
+    if (document.getElementById("everywhere").checked) {
+        Green += 1;
+    } else if (document.getElementById("nope").checked) {
+        Pink += 1;
+        Purple += 1;
+        Blue += 1;
     } else {
-        Chicago += 1;
+        Orange += 1;
+        Yellow += 1;
+        Red += 1;
+    }
+    
+    if (document.getElementById("yeah").checked) {
+        Purple += 1;
+        Pink += 1;
+        Orange += 1;
+    } else if (document.getElementById("nah").checked) {
+        Green += 1;
+        Blue += 1;
+        Yellow += 1;
+    } else {
+        Red += 1;
+    }
+    
+    if (document.getElementById("yes").checked) {
+        Purple += 1;
+        Green += 1;
+        Orange -= 8;
+        Red -= 8;
+        Pink -= 8;
+        Orange -= 8;
+        Yellow -= 8;
+        Blue += 1;
+    } else if (document.getElementById("no").checked) {
+        Red += 1;
+        Pink += 1;
+        Orange += 1;
+        Yellow += 1;
+        Green -= 8;
+    } else {
+        Blue += 1;
+        Purple += 1;
+    }
+
+    if (document.getElementById("boy").checked) {
+        Blue += 3;
+        Red += 1;
+        Orange += 1;
+        Green += 1;
+    } else if (document.getElementById("girl").checked) {
+        Pink += 3;
+        Purple += 1;
+        Yellow += 1;
+    } else {
+        Red += 0;
+    }
+
+    if (document.getElementById("yea").checked) {
+        Orange += 1;
+    } else if (document.getElementById("naha").checked) {
+        Green += 1;
+        Blue += 1;
+        Pink += 1;
+    } else if (document.getElementById("ish").checked) {
+        Yellow += 1;
+        Red += 1;
+    } else {
+        Purple += 1;
+
     }
 
     resetResult();
 
-    if (LA > Seattle && LA > Miami && LA > Chicago) {
-        document.getElementById("LAResult").style.display = "block";
-    } else if (Seattle > LA && Seattle > Miami && Seattle > Chicago) {
-        document.getElementById("SeattleResult").style.display = "block";
-    } else if (Miami > LA && Miami > Seattle && Miami > Chicago) {
-        document.getElementById("MiamiResult").style.display = "block";
+    if (Red > Orange && Red > Yellow && Red > Green && Red > Blue && Red > Purple && Red > Pink) {
+        document.getElementById("RedResult").style.display = "block";
+    } else if (Green > Red && Green > Orange && Green > Purple && Green > Yellow && Green > Blue && Green> Pink) {
+        document.getElementById("GreenResult").style.display = "block";
+    } else if (Blue > Red && Blue > Green && Blue > Orange && Blue > Purple && Blue > Yellow && Blue > Pink) {
+        document.getElementById("BlueResult").style.display = "block";
+    } else if (Orange > Red && Orange > Green && Orange > Purple && Orange > Yellow && Orange > Green && Orange > Pink) {
+        document.getElementById("OrangeResult").style.display = "block";
+    } else if (Yellow > Red && Yellow > Green && Yellow > Orange && Yellow > Purple && Yellow > Blue && Yellow > Pink) {
+        document.getElementById("YellowResult").style.display = "block";
+    } else if (Purple > Red && Purple > Green && Purple > Orange && Purple > Yellow && Purple > Blue && Purple > Pink) {
+        document.getElementById("PurpleResult").style.display = "block";
     } else {
-        document.getElementById("ChicagoResult").style.display = "block";
+        document.getElementById("PinkResult").style.display = "block";
     }
 }
